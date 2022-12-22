1c7fd6156bd875cdffab567658203086b3f67f09
osd-02 <sakanaq0223@gmail.com>
2022-12-22 23:01:51 +0900

    add create update

--- app/controllers/recommend_board_games_controller.rb
+++ app/controllers/recommend_board_games_controller.rb
@@ -6,4 +6,37 @@ class RecommendBoardGamesController < ApplicationController
   def show
     @recommend_board_game = RecommendBoardGame.find(params[:id])
   end
+
+  def new
+    @recommend_board_game = RecommendBoardGame.new
+  end
+
+  def create
+    @recommend_board_game = RecommendBoardGame.new(title: "hoge", link: "hoge", score: 100, body: "hogehoge")
+
+    if @recommend_board_game.save
+      redirect_to @recommend_board_game
+    else
+        render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @recommend_board_game = RecommendBoardGame.find(params[:id])
+  end
+
+  def update
+    @recommend_board_game = RecommendBoardGame.find(params[:id])
+
+    if @recommend_board_game.update(recommend_board_game_params)
+      redirect_to @recommend_board_game
+    else
+        render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def recommend_board_game_params
+      params.require(:recommend_board_game).permit(:title, :link, :score, :body)
+    end
 end
