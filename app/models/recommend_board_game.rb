9d28efd800d20ce04de9d892296a386197c09f64
osd-02 <sakanaq0223@gmail.com>
2022-12-24 01:36:26 +0900

    revise

--- app/models/recommend_board_game.rb
+++ app/models/recommend_board_game.rb
@@ -1,7 +1,7 @@
 class RecommendBoardGame < ApplicationRecord
   has_many :comment_to_recommends
 
-  validates :title, presence: true, length: {maximum: 20}
+  validates :title, presence: true
   validates :link, presence: true
   validates :score, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100, only_integer: true}
   validates :body, presence: true
